b2b59f9cba2855513ba6a91327a4bcd027292792
exesalle <exesalle@gmail.com>
2023-03-23 17:28:04 +0300

    .

--- src/pages/admin/adminCourses.tsx
+++ src/pages/admin/adminCourses.tsx
@@ -1,19 +1,19 @@
-import React, {useEffect, useState} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import AdminMenu from '../../components/AdminMenu';
-import {collection, getDocs, addDoc, getFirestore, onSnapshot} from 'firebase/firestore';
+import {collection, getDocs, addDoc, getFirestore, onSnapshot, doc, query, DocumentData} from 'firebase/firestore';
 import {ICoursesData, InitialCourses} from '../../Types';
 
 
-const AdminCourses = () => {
+const AdminCourses:FC = () => {
 
   const db = getFirestore();
-  const [courseData,setCourseData] = useState<ICoursesData>({
+  const [courseData, setCourseData] = useState<ICoursesData>({
     id: 1,
     name: '',
     teacher: ''
   });
 
-  const [coursesData,setCoursesData] = useState<ICoursesData[]>(InitialCourses);
+  const [coursesData, setCoursesData] = useState<ICoursesData[]>(InitialCourses);
 
   const addCourse = async () => {
     try {
@@ -28,12 +28,20 @@ const AdminCourses = () => {
   };
 
 
-  const displayCourses = onSnapshot(collection(db, 'courses'), doc => {
-    doc.forEach((d: any) => {
-      setCoursesData(prev => [...prev, d.data()]);
-    });
-  });
-
+  useEffect(() => {
+    const displayCourses = async () => {
+      const q = query(collection(db, 'courses'));
+      const querySnapshot = await getDocs(q);
+      await querySnapshot.forEach((doc:any) => {
+      // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, ' => ', doc.data());
+        setCoursesData(prev => [...prev, doc.data()]);
+      });
+    };
+    return () => {
+      displayCourses();
+    };
+  }, []);
 
 
   return (
